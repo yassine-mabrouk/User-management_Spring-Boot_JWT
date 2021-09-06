c4611f8e3d8abce7ea978200bfbd7066ca3e0244
Yassine Mabrouk <y.mabrouk@slokware.com>
2021-09-06 17:42:40 +0100

    install spring boot project

---
+++ src/main/java/com/enset/fbc/Application.java
@@ -0,0 +1,22 @@
+package com.enset.fbc;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+@SpringBootApplication
+@Configuration
+public class Application implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
