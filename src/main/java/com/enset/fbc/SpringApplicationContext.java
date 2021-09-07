6a442424038b901c7394dfe6c3627cec86777b65
Yassine Mabrouk <y.mabrouk@slokware.com>
2021-09-07 02:58:55 +0100

    security with spring security and JWT

--- src/main/java/com/enset/fbc/SpringApplicationContext.java
+++ src/main/java/com/enset/fbc/SpringApplicationContext.java
@@ -9,7 +9,6 @@ public class SpringApplicationContext  implements ApplicationContextAware {
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        // TODO Auto-generated method stub
         CONTEXT = applicationContext;
     }
 
